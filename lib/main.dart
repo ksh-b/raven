0ad2866facd0a1cdfe6eae6cd7b3b513d4e88261
kshib <ksyko@pm.me>
2023-12-25 21:21:06 +0530

    move main file

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:whapp/model/user_subscription.dart';
+import 'package:whapp/pages/home.dart';
+
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserSubscriptionAdapter());
+  await Hive.openBox('subscriptions');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:whapp/pages/main.dart';
+import 'package:whapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
