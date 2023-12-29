2b27851b96d19bc26c0892b1c728333edfc42b83
kshib <ksyko@pm.me>
2023-12-29 15:41:24 +0530

    use store

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:whapp/model/article.dart';
 import 'package:whapp/model/publisher.dart';
-import 'package:whapp/model/user_subscription.dart';
 
 import 'package:whapp/pages/full_article.dart';
 import 'package:whapp/utils/store.dart';
@@ -46,75 +45,79 @@ class _FeedPageState extends State<FeedPage>
             });
             _loadMoreItems();
           },
-          child:ValueListenableBuilder(
+          child: ValueListenableBuilder(
             valueListenable: Store.subscriptions.listenable(),
             builder: (BuildContext context, box, Widget? child) {
-              if(Store.selectedSubscriptions.isNotEmpty) {
+              if (Store.selectedSubscriptions.isNotEmpty) {
                 return ListView.builder(
-                itemCount: filteredArticles.length + 2,
-                itemBuilder: (context, index) {
-                  if(index==0){
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        controller: searchController,
-                        onChanged: (value) {
-                          setState(() {
-                            filteredArticles = newsArticles
-                                .where((article) =>
-                                article!.title.toLowerCase().contains(value.toLowerCase()))
-                                .toList();
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Search feed',
-                          prefixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(30.0), // Adjust the value as needed
+                  itemCount: filteredArticles.length + 2,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: searchController,
+                          onChanged: (value) {
+                            setState(() {
+                              filteredArticles = newsArticles
+                                  .where((article) => article!.title
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()))
+                                  .toList();
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Search feed',
+                            prefixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(
+                                    30.0), // Adjust the value as needed
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  }
-                  else if (index-1 < filteredArticles.length) {
-                    var article = filteredArticles[index-1];
-                    return ListTile(
-                      title: Text(article!.title),
-                      leading: CachedNetworkImage(
-                        imageUrl: article.publisher.iconUrl,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) {
-                          return CircularProgressIndicator(
-                              value: downloadProgress.progress);
+                      );
+                    } else if (index - 1 < filteredArticles.length) {
+                      var article = filteredArticles[index - 1];
+                      return ListTile(
+                        title: Text(article!.title),
+                        leading: CachedNetworkImage(
+                          imageUrl: article.publisher.iconUrl,
+                          progressIndicatorBuilder:
+                              (context, url, downloadProgress) {
+                            return CircularProgressIndicator(
+                                value: downloadProgress.progress);
+                          },
+                          height: 24,
+                          width: 24,
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                        subtitle: Text(
+                          "${article.publishedAt.value}",
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    ArticlePage(article: article)),
+                          );
                         },
-                        errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                      ),
-                      subtitle: Text(
-                        "${article.publishedAt.value}",
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  ArticlePage(article: article)),
-                        );
-                      },
-                    );
-                  } else {
-                    return ElevatedButton(
-                      onPressed: () {
-                        _loadMoreItems();
-                      },
-                      child: const Text("Load more"),
-                    );
-                  }
-                },
-              );
-              } return Center(child: Text("Select some subscriptions"));
+                      );
+                    } else {
+                      return ElevatedButton(
+                        onPressed: () {
+                          _loadMoreItems();
+                        },
+                        child: const Text("Load more"),
+                      );
+                    }
+                  },
+                );
+              }
+              return Center(child: Text("Select some subscriptions"));
             },
           ),
         ),
