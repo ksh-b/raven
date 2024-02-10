4aff2650d52f919a9389bace6fc288feedf73fbf
kshib <ksyko@pm.me>
2024-02-10 15:46:21 +0530

    add/fix time parser

--- lib/utils/time.dart
+++ lib/utils/time.dart
@@ -2,46 +2,66 @@ import 'package:intl/intl.dart';
 
 MapEntry<int, String> parseDateString(String timestamp) {
   try {
-    if(timestamp.contains("ago")) {
-      if(timestamp.startsWith("a ")) {
-        timestamp = timestamp.replaceFirst("a ", "1 ");
-      }
-      return MapEntry(convertTimeStringToSeconds(timestamp), timestamp);
-    }
-    DateTime dateTime = DateTime.parse(timestamp);
+    int differenceInSeconds = calculateDifferenceInSeconds(timestamp);
+    return formatTimeDifference(differenceInSeconds);
+  } catch (e) {
+    return MapEntry(0, timestamp);
+  }
+}
+
+MapEntry<int, String> parseUnixTime(int unixTime) {
+  try {
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(unixTime);
     DateTime now = DateTime.now();
     int differenceInSeconds = now.difference(dateTime).inSeconds;
-    const int minute = 60;
-    const int hour = 60 * minute;
-    const int day = 24 * hour;
-    const int month = 30 * day;
-
-    if (differenceInSeconds < 1) {
-      return MapEntry(differenceInSeconds, 'just now');
-    } else if (differenceInSeconds < minute) {
-      return MapEntry(differenceInSeconds, '$differenceInSeconds seconds ago');
-    } else if (differenceInSeconds < hour) {
-      int minutes = (differenceInSeconds / minute).floor();
-      return MapEntry(differenceInSeconds,
-          '$minutes ${(minutes == 1) ? 'minute' : 'minutes'} ago');
-    } else if (differenceInSeconds < day) {
-      int hours = (differenceInSeconds / hour).floor();
-      return MapEntry(
-          differenceInSeconds, '$hours ${(hours == 1) ? 'hour' : 'hours'} ago');
-    } else if (differenceInSeconds < month) {
-      int days = (differenceInSeconds / day).floor();
-      if (days == 1) {
-        return MapEntry(differenceInSeconds, 'yesterday');
-      } else {
-        return MapEntry(differenceInSeconds, '$days days ago');
-      }
-    } else {
-      int months = (differenceInSeconds / month).floor();
-      return MapEntry(differenceInSeconds,
-          '$months ${(months == 1) ? 'month' : 'months'} ago');
-    }
+    return formatTimeDifference(differenceInSeconds);
   } catch (e) {
-    return MapEntry(0, timestamp); // Return 0 differenceInSeconds in case of an error.
+    return MapEntry(0, unixTime.toString());
+  }
+}
+
+int calculateDifferenceInSeconds(String timestamp) {
+  if (timestamp.contains("ago")) {
+    if (timestamp.startsWith("a ")) {
+      timestamp = timestamp.replaceFirst("a ", "1 ");
+    }
+    return convertTimeStringToSeconds(timestamp);
+  } else {
+    DateTime dateTime = DateTime.parse(timestamp);
+    DateTime now = DateTime.now();
+    return now.difference(dateTime).inSeconds;
+  }
+}
+
+MapEntry<int, String> formatTimeDifference(int differenceInSeconds) {
+  const int minute = 60;
+  const int hour = 60 * minute;
+  const int day = 24 * hour;
+  const int month = 30 * day;
+
+  if (differenceInSeconds < 1) {
+    return MapEntry(differenceInSeconds, 'just now');
+  } else if (differenceInSeconds < minute) {
+    return MapEntry(differenceInSeconds, '$differenceInSeconds seconds ago');
+  } else if (differenceInSeconds < hour) {
+    int minutes = (differenceInSeconds / minute).floor();
+    return MapEntry(differenceInSeconds,
+        '$minutes ${(minutes == 1) ? 'minute' : 'minutes'} ago');
+  } else if (differenceInSeconds < day) {
+    int hours = (differenceInSeconds / hour).floor();
+    return MapEntry(
+        differenceInSeconds, '$hours ${(hours == 1) ? 'hour' : 'hours'} ago');
+  } else if (differenceInSeconds < month) {
+    int days = (differenceInSeconds / day).floor();
+    if (days == 1) {
+      return MapEntry(differenceInSeconds, 'yesterday');
+    } else {
+      return MapEntry(differenceInSeconds, '$days days ago');
+    }
+  } else {
+    int months = (differenceInSeconds / month).floor();
+    return MapEntry(differenceInSeconds,
+        '$months ${(months == 1) ? 'month' : 'months'} ago');
   }
 }
 
