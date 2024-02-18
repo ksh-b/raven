0f7c332bf9a3e7602b29db59e39b5ee75bb69b6f
kshib <ksyko@pm.me>
2024-02-18 12:24:41 +0530

    add simplytranslate api

---
+++ lib/api/simplytranslate.dart
@@ -0,0 +1,188 @@
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
+
+class SimplyTranslate {
+  Map<String, String> languages = {
+    "Afrikaans": "af",
+    "Albanian": "sq",
+    "Amharic": "am",
+    "Arabic": "ar",
+    "Armenian": "hy",
+    "Assamese": "as",
+    "Aymara": "ay",
+    "Azerbaijani": "az",
+    "Bambara": "bm",
+    "Basque": "eu",
+    "Belarusian": "be",
+    "Bengali": "bn",
+    "Bhojpuri": "bho",
+    "Bosnian": "bs",
+    "Bulgarian": "bg",
+    "Catalan": "ca",
+    "Cebuano": "ceb",
+    "Chichewa": "ny",
+    "Chinese (Simplified)": "zh-CN",
+    "Chinese (Traditional)": "zh-TW",
+    "Corsican": "co",
+    "Croatian": "hr",
+    "Czech": "cs",
+    "Danish": "da",
+    "Dhivehi": "dv",
+    "Dogri": "doi",
+    "Dutch": "nl",
+    "English": "en",
+    "Esperanto": "eo",
+    "Estonian": "et",
+    "Ewe": "ee",
+    "Filipino": "tl",
+    "Finnish": "fi",
+    "French": "fr",
+    "Frisian": "fy",
+    "Galician": "gl",
+    "Georgian": "ka",
+    "German": "de",
+    "Greek": "el",
+    "Guarani": "gn",
+    "Gujarati": "gu",
+    "Haitian Creole": "ht",
+    "Hausa": "ha",
+    "Hawaiian": "haw",
+    "Hebrew": "iw",
+    "Hindi": "hi",
+    "Hmong": "hmn",
+    "Hungarian": "hu",
+    "Icelandic": "is",
+    "Igbo": "ig",
+    "Ilocano": "ilo",
+    "Indonesian": "id",
+    "Irish": "ga",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Javanese": "jw",
+    "Kannada": "kn",
+    "Kazakh": "kk",
+    "Khmer": "km",
+    "Kinyarwanda": "rw",
+    "Konkani": "gom",
+    "Korean": "ko",
+    "Krio": "kri",
+    "Kurdish (Kurmanji)": "ku",
+    "Kurdish (Sorani)": "ckb",
+    "Kyrgyz": "ky",
+    "Lao": "lo",
+    "Latin": "la",
+    "Latvian": "lv",
+    "Lingala": "ln",
+    "Lithuanian": "lt",
+    "Luganda": "lg",
+    "Luxembourgish": "lb",
+    "Macedonian": "mk",
+    "Maithili": "mai",
+    "Malagasy": "mg",
+    "Malay": "ms",
+    "Malayalam": "ml",
+    "Maltese": "mt",
+    "Maori": "mi",
+    "Marathi": "mr",
+    "Meiteilon (Manipuri)": "mni-Mtei",
+    "Mizo": "lus",
+    "Mongolian": "mn",
+    "Myanmar (Burmese)": "my",
+    "Nepali": "ne",
+    "Norwegian": "no",
+    "Odia (Oriya)": "or",
+    "Oromo": "om",
+    "Pashto": "ps",
+    "Persian": "fa",
+    "Polish": "pl",
+    "Portuguese": "pt",
+    "Punjabi": "pa",
+    "Quechua": "qu",
+    "Romanian": "ro",
+    "Russian": "ru",
+    "Samoan": "sm",
+    "Sanskrit": "sa",
+    "Scots Gaelic": "gd",
+    "Sepedi": "nso",
+    "Serbian": "sr",
+    "Sesotho": "st",
+    "Shona": "sn",
+    "Sindhi": "sd",
+    "Sinhala": "si",
+    "Slovak": "sk",
+    "Slovenian": "sl",
+    "Somali": "so",
+    "Spanish": "es",
+    "Sundanese": "su",
+    "Swahili": "sw",
+    "Swedish": "sv",
+    "Tajik": "tg",
+    "Tamil": "ta",
+    "Tatar": "tt",
+    "Telugu": "te",
+    "Thai": "th",
+    "Tigrinya": "ti",
+    "Tsonga": "ts",
+    "Turkish": "tr",
+    "Turkmen": "tk",
+    "Twi": "ak",
+    "Ukrainian": "uk",
+    "Urdu": "ur",
+    "Uyghur": "ug",
+    "Uzbek": "uz",
+    "Vietnamese": "vi",
+    "Welsh": "cy",
+    "Xhosa": "xh",
+    "Yiddish": "yi",
+    "Yoruba": "yo",
+    "Zulu": "zu"
+  };
+
+  Future<String> translate(String inputText, String language) async {
+    if(!languages.containsKey(language)) {
+      return inputText;
+    }
+    String translatedText ="";
+    String url = 'https://simplytranslate.org/?engine=google';
+    inputText = removeHtmlAttributes(inputText);
+    List<String> inputTextParts = splitString(inputText, 500);
+    for (String inputTextPart in inputTextParts) {
+      Map<String, String> payload = {
+        'from': 'auto',
+        'to': languages[language]!,
+        'text': inputTextPart
+      };
+      var response = await http.post(Uri.parse(url), body: payload);
+      var document = parse(response.body);
+      translatedText += document
+          .getElementById('output')
+          ?.text ?? "";
+    }
+    return translatedText;
+  }
+
+  List<String> splitString(String text, int wordsPerString) {
+    List<String> result = [];
+    List<String> words = text.split(' ');
+    int start = 0;
+
+    while (start < words.length) {
+      int end = start + wordsPerString;
+      if (end > words.length) {
+        end = words.length;
+      }
+      result.add(words.sublist(start, end).join(' '));
+      start = end;
+    }
+
+    return result;
+  }
+  String removeHtmlAttributes(String htmlString) {
+    RegExp exp = RegExp(r'<[^>]+>');
+    return htmlString.replaceAllMapped(exp, (match) {
+      String tag = match.group(0)!;
+      return tag.replaceAll(RegExp(r'\s\S+?="[^"]*?"'), ''); // Removes attributes
+    });
+  }
+
+}
