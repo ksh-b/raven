32ba6a04c38f557b3b776a9f29bc6da71fd52ec0
kshib <ksyko@pm.me>
2024-03-10 18:38:03 +0530

    remove unnecessary ?

--- lib/model/publisher.dart
+++ lib/model/publisher.dart
@@ -37,17 +37,17 @@ abstract class Publisher {
 
   String get mainCategory;
 
-  Future<Set<NewsArticle?>> articles({String category = "All", int page = 1}) {
+  Future<Set<NewsArticle>> articles({String category = "All", int page = 1}) {
     return category.startsWith("#")
         ? searchedArticles(searchQuery: getAsSearchQuery(category), page: page)
         : categoryArticles(category: category, page: page);
   }
 
-  Future<Set<NewsArticle?>> categoryArticles({String category = "All", int page = 1});
+  Future<Set<NewsArticle>> categoryArticles({String category = "All", int page = 1});
 
-  Future<Set<NewsArticle?>> searchedArticles({required String searchQuery, int page = 1});
+  Future<Set<NewsArticle>> searchedArticles({required String searchQuery, int page = 1});
 
-  Future<NewsArticle?> article(NewsArticle newsArticle);
+  Future<NewsArticle> article(NewsArticle newsArticle);
 
   Map<String, dynamic> toJson() {
     return {
