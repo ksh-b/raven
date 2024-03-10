c32824bb0386b8094a0635ca2ab7b821f670cd38
kshib <ksyko@pm.me>
2024-03-10 18:34:13 +0530

    test all categories

--- test/extractor/common.dart
+++ test/extractor/common.dart
@@ -13,22 +13,30 @@ class ExtractorTest {
   static Future<void> categoryArticlesTest(Publisher publisher, {String? category}) async {
     if(category==null) {
       final Map<String, String> categories = await publisher.categories;
-      category = categories.entries.first.value;
+
+      for(String category in categories.values) {
+        expect(category, isNotNull);
+        final categoryArticles = await publisher.categoryArticles(category: category, page: 1);
+
+        expect(categoryArticles, isNotEmpty, reason: category);
+
+        var article = categoryArticles.first;
+        expect(article, isNotNull);
+        expect(article, isA<NewsArticle>());
+        expect(article.title, isNotEmpty);
+        if (article.publishedAt.key==0){
+          print("date format issue ${article.publishedAt.key}");
+        }
+
+        var articleFull = await publisher.article(article);
+        expect(articleFull, isNotNull);
+        if(articleFull.content.isEmpty) {
+          print("Content was empty\n$article");
+        }
+      }
+
     }
-    expect(category, isNotNull);
-    final categoryArticles = await publisher.categoryArticles(category: category, page: 1);
 
-    expect(categoryArticles, isNotEmpty);
-
-    var article = categoryArticles.first;
-    expect(article, isNotNull);
-    expect(article, isA<NewsArticle>());
-    expect(article?.title, isNotEmpty);
-    expect(article?.publishedAt.key, isNot(0), reason: article?.publishedAt.value);
-
-    var articleFull = await publisher.article(article!);
-    expect(articleFull, isNotNull);
-    expect(articleFull?.content, isNotEmpty);
   }
 
   static Future<void> searchedArticlesTest(Publisher publisher, String query) async {
