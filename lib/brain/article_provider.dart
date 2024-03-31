0c2ad7a8070b809b6f034ead9422926117017964
kshib <ksyko@pm.me>
2024-03-31 10:50:53 +0530

    load articles in parallel

--- lib/brain/article_provider.dart
+++ lib/brain/article_provider.dart
@@ -1,5 +1,4 @@
 import 'dart:collection';
-import 'package:raven/brain/worker.dart';
 import 'package:raven/model/article.dart';
 import 'package:raven/model/publisher.dart';
 import 'package:raven/model/user_subscription.dart';
@@ -10,63 +9,88 @@ class ArticleProvider {
   Set<NewsArticle> stashedArticles = {};
   HashMap<String, int> nextPage = HashMap();
 
+  void reset() {
+    stashedArticles = {};
+    nextPage = HashMap();
+  }
+
   Future<List<NewsArticle>> loadPage(int page, {String? query}) async {
+    List<Future<Set<NewsArticle>>> futures = [];
     List<NewsArticle> newsArticles = [];
     List<NewsArticle> subscriptionArticles = [];
     List<UserSubscription> subscriptions = Store.selectedSubscriptions;
-    final worker = await Worker.spawn();
+    bool needFresh = false;
 
     // Load user subscriptions (Publisher+Category)
     for (var subscription in subscriptions) {
+      
       // get publisher for this subscription
       Publisher publisher = publishers[subscription.publisher]!;
-
       // if there are any articles in stash, un-stash them
       var stashedPublisherArticles =
           stashedArticles.where((e) => e.publisher == publisher).toList();
+      
+          "Loading page $page for $subscription, stashed: ${stashedPublisherArticles.length}");
       if (stashedPublisherArticles.isNotEmpty) {
         subscriptionArticles = stashedPublisherArticles.take(few).toList();
+        
         stashedArticles.removeAll(subscriptionArticles);
       }
 
       // else get fresh articles for this subscription
       else {
+        needFresh = true;
         int page_ = nextPage.containsKey(subscription.toString())
             ? nextPage[subscription.toString()]!
             : page;
 
         if (query != null) {
-          await worker.parseArticles(subscription.publisher, page_, query, true)
-              .then((articles) {
-            if (articles is Set<NewsArticle>) {
-              collectPublisherArticles(subscriptionArticles, articles, subscription, page);
-            }
-          });
+          futures.add(
+            publisher.searchedArticles(
+              searchQuery: query,
+              page: page_,
+            ),
+          );
         } else {
-          await worker.parseArticles(subscription.publisher, page_, subscription.category, false)
-              .then((articles) {
-            if (articles is Set<NewsArticle>) {
-              collectPublisherArticles(subscriptionArticles, articles, subscription, page);
-            }
-          });
+          futures.add(
+            publisher.categoryArticles(
+              category: subscription.category,
+              page: page_,
+            ),
+          );
         }
       }
+      
     }
-    worker.close();
+    if (needFresh) {
+      await Future.wait(futures).then((values) {
+        for (Set<NewsArticle> value in values) {
+          collectPublisherArticles(
+            subscriptionArticles,
+            value,
+            "${value.first.publisher.name}~${value.first.category}",
+            page,
+          );
+        }
+      });
+    }
     newsArticles.addAll(subscriptionArticles);
     return newsArticles;
   }
 
-  void collectPublisherArticles(List<NewsArticle> subscriptionArticles,
-      Set<NewsArticle> articles, subscription, int page) {
+  void collectPublisherArticles(
+    List<NewsArticle> subscriptionArticles,
+    Set<NewsArticle> articles,
+    String subscription,
+    int page,
+  ) {
     subscriptionArticles.addAll(articles.take(few).toList());
     var stashedArticles_ = articles.skip(few);
     stashedArticles.addAll(stashedArticles_);
     if (stashedArticles_.isNotEmpty) {
-      nextPage[subscription.toString()] =
-          nextPage.containsKey(subscription.toString())
-              ? (nextPage[subscription.toString()]! + 1)
-              : page + 1;
+      nextPage[subscription] = nextPage.containsKey(subscription)
+          ? (nextPage[subscription]! + 1)
+          : page + 1;
     }
 
     // sort - show most recent first
