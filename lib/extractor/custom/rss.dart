49f5e684d0383652a7ae38d14fbcdc6d907880b3
kshib <ksyko@pm.me>
2024-05-05 23:26:20 +0530

    handle each feed type separately

--- lib/extractor/custom/rss.dart
+++ lib/extractor/custom/rss.dart
@@ -6,6 +6,7 @@ import 'package:raven/api/smort.dart';
 import 'package:raven/brain/html_content_extractor.dart';
 import 'package:raven/model/article.dart';
 import 'package:raven/model/publisher.dart';
+import 'package:raven/utils/time.dart';
 
 class RSSFeed extends Publisher {
   @override
@@ -52,21 +53,83 @@ class RSSFeed extends Publisher {
     final response = await http.get(Uri.parse(category));
     if (response.statusCode == 200) {
       WebFeed feed = WebFeed.fromXmlString(response.body);
-      for (var item in feed.items) {
-        articles.add(
-          NewsArticle(
-            publisher: name,
-            title: item.title,
-            content: item.body,
-            excerpt: "",
-            author: "",
-            url: item.links.first ?? "",
-            tags: [],
-            thumbnail: "",
-            publishedAt: item.updated?.millisecondsSinceEpoch ?? -1,
-            category: category,
-          ),
-        );
+      RssFeed rssFeed = RssFeed();
+      Rss1Feed rss1Feed = Rss1Feed();
+      AtomFeed atomFeed = AtomFeed();
+      if (feed.rssVersion == RssVersion.rss2) {
+        rssFeed = RssFeed.parse(response.body);
+        for (var item in rssFeed.items) {
+          articles.add(
+            NewsArticle(
+              publisher: name,
+              title: item.title ?? "",
+              content: item.content?.value ?? "",
+              excerpt: item.description ?? "",
+              author: item.author ?? "",
+              url: item.link ?? "",
+              tags: item.categories.map((e) => e.value ?? "").toList(),
+              thumbnail: item.content?.images.first ?? "",
+              publishedAt:
+                  item.pubDate != null ? stringToUnix(item.pubDate!) : -1,
+              category: category,
+            ),
+          );
+        }
+      } else if (feed.rssVersion == RssVersion.atom) {
+        atomFeed = AtomFeed.parse(response.body);
+        for (var item in atomFeed.items) {
+          articles.add(
+            NewsArticle(
+              publisher: name,
+              title: item.title ?? "",
+              content: item.content ?? "",
+              excerpt: item.summary ?? "",
+              author: item.authors.join(", "),
+              url: item.links.first.href ?? "",
+              tags: item.categories.map((e) => e.label ?? "").toList(),
+              thumbnail: item.media?.thumbnails.first.url ?? "",
+              publishedAt:
+                  item.published != null ? stringToUnix(item.published!) : -1,
+              category: category,
+            ),
+          );
+        }
+      } else if (feed.rssVersion == RssVersion.rss1) {
+        rss1Feed = Rss1Feed.parse(response.body);
+        for (var item in rss1Feed.items) {
+          articles.add(
+            NewsArticle(
+              publisher: name,
+              title: item.title ?? "",
+              content: item.content?.value ?? "",
+              excerpt: item.description ?? "",
+              author: item.dc?.contributor ?? "",
+              url: item.link ?? "",
+              tags: item.dc?.subjects ?? [],
+              thumbnail: item.content?.images.first ?? "",
+              publishedAt:
+                  item.dc?.date != null ? stringToUnix(item.dc!.date!) : -1,
+              category: category,
+            ),
+          );
+        }
+      } else {
+        for (var item in feed.items) {
+          articles.add(
+            NewsArticle(
+              publisher: name,
+              title: item.title,
+              content: item.body,
+              excerpt: "",
+              author: "",
+              url: item.links.first ?? "",
+              tags: [],
+              thumbnail: "",
+              publishedAt: item.updated?.millisecondsSinceEpoch ?? -1,
+              category: category,
+            ),
+          );
+        }
       }
     }
     return articles;
