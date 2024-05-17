e27a7f46278718d2458b90267b7f7095f1494125
kshib <ksyko@pm.me>
2024-05-18 01:30:40 +0530

    complete url

--- lib/brain/fallback_provider.dart
+++ lib/brain/fallback_provider.dart
@@ -1,16 +1,22 @@
 import 'package:raven/model/article.dart';
+import 'package:raven/model/publisher.dart';
 import 'package:raven/service/html_content_extractor.dart';
 import 'package:raven/service/smort.dart';
 
 class FallbackProvider {
   Future<NewsArticle> get(NewsArticle article) async {
-      var maybeArticle = await HtmlContentExtractor().fallback(article);
-      if (!maybeArticle.key) {
-        maybeArticle = await Smort().fallback(article);
-      }
-      if (!maybeArticle.key) {
-        return article;
-      }
-      return maybeArticle.value;
+    if (!article.url.contains(publishers[article.publisher]!.homePage)) {
+      var homePage = publishers[article.publisher]!.homePage;
+      article.url = homePage+article.url;
+    }
+
+    var maybeArticle = await HtmlContentExtractor().fallback(article);
+    if (!maybeArticle.key) {
+      maybeArticle = await Smort().fallback(article);
+    }
+    if (!maybeArticle.key) {
+      return article;
+    }
+    return maybeArticle.value;
   }
 }
