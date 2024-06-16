f79cbb0056a28be04a65e31a8477239f1b8fb44f
kshib <ksyko@pm.me>
2024-06-16 17:00:10 +0530

    add findStringBetween

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -14,3 +14,15 @@ String createTag(String inputString) {
 bool isNumeric(String str) {
   return double.tryParse(str) != null;
 }
+
+String findStringBetween(String text, String start, String end) {
+  text = text.replaceAll(RegExp("\\s+"), " ");
+  int startIndex = text.indexOf(start);
+  int endIndex = text.indexOf(end, startIndex + start.length);
+
+  if (startIndex != -1 && endIndex != -1) {
+    return text.substring(startIndex + start.length, endIndex);
+  } else {
+    return "";
+  }
+}
