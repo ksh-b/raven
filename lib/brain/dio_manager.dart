62b40aaca590148b8a59a36050d889f705736397
kshib <ksyko@pm.me>
2024-07-08 22:27:42 +0530

    add request retry

--- lib/brain/dio_manager.dart
+++ lib/brain/dio_manager.dart
@@ -1,5 +1,20 @@
 import 'package:dio/dio.dart';
+import 'package:dio_smart_retry/dio_smart_retry.dart';
 
 Dio dio() {
-  return Dio()..options = BaseOptions(validateStatus: (status) => true);
+  final dio_ = Dio();
+  dio_.options = BaseOptions(validateStatus: (status) => true);
+  dio_.interceptors.add(
+    RetryInterceptor(
+      dio: dio_,
+      logPrint: print,
+      retries: 3,
+      retryDelays: const [
+        Duration(seconds: 2),
+        Duration(seconds: 3),
+        Duration(seconds: 5),
+      ],
+    ),
+  );
+  return dio_;
 }
